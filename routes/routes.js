4b18f372b2135880ff01dcfddfc9cc2919688f86
Anandhu-Manoj <manojanandhu74@gmail.com>
2025-03-26 11:35:17 +0530

    corrected

--- routes/routes.js
+++ routes/routes.js
@@ -1,5 +1,5 @@
 const express=require('express')
 const productController=require('..//controllers/productControllers')
 const router=new express.Router()
-router.post('/add',productController,addController)
+router.post('/add',productController.addController)
 module.exports=router
